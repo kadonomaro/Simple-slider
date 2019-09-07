9e31beee574a373c363b8191cea5671c0b2a62f6
kadonomaro <denis.afer@gmail.com>
2019-09-07 21:28:04 +0700

    added slider nav

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ let imageSlider = document.querySelector('.images');
 let slider = new SimpleSlider({
     selector: imageSlider,
     nav: true,
+    navClass: 'prev next',
     transition: 'ease',
+    speed: 300,
     slideToShow: 3,
 });
 slider.init();
--- libs/simpleSlider/simple-slider.js
+++ libs/simpleSlider/simple-slider.js
@@ -2,33 +2,46 @@ export default class SimpleSlider {
     constructor(options) {
         this.selector = options.selector;
         this.slideToShow = options.slideToShow || 3;
+        this.slideToScroll = options.slideToScroll || 1;
         this.startFrom = options.startFrom || 'first'; // first, center, last
         this.nav = options.nav || true;
+        this.navClass = options.navClass || '';
         this.dots = options.dots || false;
         this.dotsClass = options.dotsClass || '';
         this.transition = options.transition || 'linear';
+        this.speed = options.speed || 300;
     }
     
     init() {
-        console.dir(this);
+
         let sliderTrack = document.createElement('div');
+        let sliderWrapper = document.createElement('div');
         let sliderLength = this.selector.children.length;
         this.selector.classList.add('simple-slider');
         sliderTrack.classList.add('simple-slider__track');
+        sliderWrapper.classList.add('simple-slider__wrapper');
+        sliderTrack.style.transition = `transform ${this.speed}ms ${this.transition}`;
 
         [...this.selector.children].forEach(child => {
-            
             
             sliderTrack.appendChild(child);
             child.style.width = 100 / sliderLength + '%';
             
-            
         });
-        this.selector.appendChild(sliderTrack);
+        this.selector.appendChild(sliderWrapper);
+        sliderWrapper.appendChild(sliderTrack);
+        // this.selector.appendChild(sliderTrack);
 
         sliderTrack.style.width = sliderTrack.children.length * 100 / this.slideToShow + '%';
 
         this.setInitialClasses(sliderTrack, 'simple-slider__slide');
+
+
+        sliderTrack.addEventListener('click', function () {
+            this.style.transform += 'translateX(-10%)'; 
+        });
+
+        this.navInit(sliderWrapper, sliderTrack);
     }
 
     
@@ -38,9 +51,36 @@ export default class SimpleSlider {
             child.classList.add(className);
         });
     }
-    setInitialState() {}
-    slidePrev() {}
-    slideNext() {}
+
+    navInit(parent, track) {
+        const navContainer = document.createElement('div');
+        const navPrevButton = document.createElement('button');
+        const navNextButton = document.createElement('button');
+        navContainer.classList.add('simple-slider__nav');
+        navPrevButton.classList.add('simple-slider__prev');
+        navNextButton.classList.add('simple-slider__next');
+        navContainer.appendChild(navPrevButton);
+        navContainer.appendChild(navNextButton);
+        parent.appendChild(navContainer);
+
+        this.slidePrev(navPrevButton, track);
+        this.slideNext(navNextButton, track);
+    }
+    setInitialState() { }
+    
+
+    slidePrev(button, track) {
+        button.addEventListener('click', function (evt) {
+            evt.preventDefault();
+            track.style.transform = `translateX(10%)`;
+        });
+    }
+    slideNext(button, track) {
+        button.addEventListener('click', function (evt) {
+            evt.preventDefault();
+            track.style.transform = `translateX(-10%)`;
+        });
+    }
 }
 
 //Подумать над длиной трека
